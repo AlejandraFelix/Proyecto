876cc431fefe078ef0e38c93e31f6d015eb888b6
AlejandraFelix <70040143+AlejandraFelix@users.noreply.github.com>
2020-08-28 22:13:22 -0500

    Add files via upload

---
+++ avance 1.py
@@ -0,0 +1,18 @@
+print("de cuanto fue tu compra")
+n=0
+p=0
+n=int(input())
+
+if (100<n<499):
+    p = p+10
+    print("estos son tus puntos:",p)
+elif (500<n<999):
+    p = p+20
+    print("estos son tus puntos;",p)
+elif (n>1000):
+    p = p+30
+    print("estos son tus puntos",p)
+    
+
+
+      
